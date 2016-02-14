ce9df9be38aede0cf7df302ecbce09073432e628
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-02-15 00:02:59 +0100

    change func-name

--- fetch/brewery_db.py
+++ fetch/brewery_db.py
@@ -2,15 +2,15 @@ import requests
 from utils import brewery_db_api_key
 
 
-def get_attributes():
-    def attributes():
+def attributes():
+    def generate_attributes():
         yield 'key', brewery_db_api_key()
         yield 'format', 'json'
-    return '&'.join('='.join(attr) for attr in attributes())
+    return '&'.join('='.join(attr) for attr in generate_attributes())
 
 
 def find_by_id(id):
-    url = 'https://api.brewerydb.com/v2/beer/{id}?'.format(id=id) + get_attributes()
+    url = 'https://api.brewerydb.com/v2/beer/{id}?'.format(id=id) + attributes()
     response = requests.get(url)
     return response.json()
 
