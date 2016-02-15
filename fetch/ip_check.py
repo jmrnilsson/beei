e64c51220e348f5df8579230aa0dd32defc27d66
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-02-16 00:56:40 +0100

    update tests. add ok_ip

--- fetch/ip_check.py
+++ fetch/ip_check.py
@@ -8,11 +8,11 @@ def check_ip():
     def map_ip(html_source):
         ip = re.findall('(?:[0-9]{1,3}\.){3}[0-9]{1,3}', html_source)
         return {
-            'ip': _zfill(ip[0]),
-            'utctime': str(datetime.utcnow())
+            'ip': ip[0],
+            'utc_datetime': str(datetime.utcnow())
         }
 
-    return http_cache.get(url_check_ip(), hint='checkip', to_json=map_ip)
+    return http_cache.get(url_check_ip(), hint='check_ip', to_json=map_ip)
 
 
 def find_all_invalid_ip_addresses():
@@ -20,21 +20,26 @@ def find_all_invalid_ip_addresses():
         return [to_ip_range(l) for l in text.splitlines() if len(l) > 10]
 
     def to_ip_range(line):
-        properties = line.split(',')
-        assigned = datetime.strptime(properties[3], '%d/%m/%y')
-
+        start, end, _, assigned, owner = line.split(',')[:5]
+        assigned = datetime.strptime(assigned, '%d/%m/%y')
+        assigned = datetime.strftime(assigned, '%Y-%m-%d')
         return {
-            'start': _zfill(properties[0]),
-            'end': _zfill(properties[1]),
-            'assigned': datetime.strftime(assigned, '%Y-%m-%d'),
-            'owner': properties[4]
+            'start': start,
+            'end': end,
+            'assigned': assigned,
+            'owner': owner
         }
 
-    return http_cache.get(url_invalid_ip_address(),
-                          cache_days=50,
-                          hint='ip',
-                          to_json=map_ip_ranges)
+    url = url_invalid_ip_address()
+    return http_cache.get(url, cache_days=50, hint='ip_list', to_json=map_ip_ranges)
 
 
-def _zfill(ip):
-    return '.'.join([number.zfill(3) for number in ip.split('.')])
+def assert_ok_ip():
+    def zfill(ip_address):
+        return '.'.join([number.zfill(3) for number in ip_address.split('.')])
+
+    ip = check_ip()['ip']
+    ip_zfill = zfill(ip)
+    for ip_range in find_all_invalid_ip_addresses():
+        if zfill(ip_range['start']) < ip_zfill < zfill(ip_range['end']):
+            raise RuntimeError('Using a void ip {} in range {}'.format(ip, ip_range['owner']))
--- test/integration/ip_check_tests.py
+++ test/integration/ip_check_tests.py
@@ -1,13 +1,17 @@
-from nose.tools import assert_greater, assert_equal
-from fetch.ip_check import find_all_invalid_ip_addresses, check_ip
+from nose.tools import assert_greater, assert_equal, assert_raises
+from fetch.ip_check import find_all_invalid_ip_addresses, check_ip, assert_ok_ip
 
 
 def test_find_all_invalid_ip_addresses():
     actual = find_all_invalid_ip_addresses()
     assert_greater(len(actual), 25)
-    assert_equal(actual[0]['start'][:1], '0')
+    assert_equal(actual[0]['start'][:2], '2.')
 
 
 def test_check_ip():
     actual = check_ip()
-    assert_equal(actual['ip'][:1], '0')
+    assert_equal(actual['ip'][:1], '8')
+
+
+def test_nok_ip():
+    assert_raises(RuntimeError, assert_ok_ip)
