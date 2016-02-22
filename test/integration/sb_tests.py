c304560afa010a1a474c551a1d2f2a5951ab92ec
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-02-22 01:44:45 +0100

    fix test

--- test/integration/sb_tests.py
+++ test/integration/sb_tests.py
@@ -3,6 +3,6 @@ from fetch import sb as client
 
 
 def test_find_by_id():
-    actual = client.find_all_by_page(1)
+    actual, _ = client.find_all_by_page(1)
     product_number = actual['ProductSearchResults'][0]['ProductNumber']
     assert_is_not_none(int(product_number))
