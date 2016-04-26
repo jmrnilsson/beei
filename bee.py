0f2a42c9c08da73164e939d66c1e9137a6ac5828
Charlotte Rising <martin.jr.nilsson@gmail.com>
2016-04-26 22:58:11 +0200

    update faulty name usage

--- bee.py
+++ bee.py
@@ -24,26 +24,24 @@ def main(sys_args):
             logger.err('ip', unicode(e.message))
             sys.exit(0)
 
-        for style in rb.index_styles(http)[:30]:
+        for style in rb.index_styles(http)[:37]:
             beers = rb.get_top_50_for_style(http, style['href'])
             for beer in beers:
                 beer_list.add(beer)
                 for b in brewery_db.find_by_name(http, beer.get('name')):
                     beer_list.add(b)
 
-        name_0, name_1 = SB_SELECTORS_NAME
-
         for i in xrange(1, 10):
             response, next_page = sb.index_api_by_page(http, i)
             sb_list = response[SB_SELECTOR_LIST]
             for beer in sb_list:
                 beer_list.add(beer)
-                for b in brewery_db.find_by_name(http, name_0):
-                    beer_list.add(b)
-                for b in brewery_db.find_by_name(http, name_1):
-                    beer_list.add(b)
-                if not next_page:
-                    break
+                names = [b for b in [beer[SB_SELECTORS_NAME[0]], beer[SB_SELECTORS_NAME[1]]] if b]
+                for name in names:
+                    for b in brewery_db.find_by_name(http, name):
+                        beer_list.add(b)
+            if not next_page:
+                break
 
         for site_map in sb.index_site_map(http):
             for site in sb.get_by_site_map(http, site_map):
