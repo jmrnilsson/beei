40a0220f45b884e789a0108dda030ddfc0e118ca
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-06-05 04:26:17 +0200

    update endies

--- beerestore.py
+++ beerestore.py
@@ -24,15 +24,6 @@ def main(sys_args):
             logger.err('ip', unicode(e.message))
             sys.exit(0)
 
-        styles = rb.index_styles(http)
-        for i, style in enumerate(styles[:1]):
-            logger.info('progress', '{} of {}'.format(i, len(styles)))
-            beers = rb.get_top_50_for_style(http, style['href'])
-            for beer in beers:
-                beer_list.add(beer)
-                for b in brewery_db.find_by_name(http, beer.get('name')):
-                    beer_list.add(b)
-
         for beer in s.api_get_all(http):
             beer_list.add(beer)
             for name in [b for b in [beer['name_0'], beer['name_1']] if b]:
